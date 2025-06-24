26c282939cac74ca87440f8e9bf81ba12704a0b8
Himanshimalviya123 <malviyahimanshi14@gmail.com>
2025-06-24 21:38:10 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -21,49 +21,119 @@
 //   }
 //   export default App;
 ////////////////////////////////////////////////////////
- import { useDispatch, useSelector } from "react-redux";
- import {addtask ,RemoveTask} from "./todoslice";
- import { useState } from "react";
-const App=()=>{
-    const work=useSelector(state=>state.todo.task);
-    const dispatch=useDispatch();
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {addTask,RemoveTask,taskComplete,taskInComplete,myEditSave,} from"./todoslice";
+function App() {
+  const [txtVal, setTxtVal] = useState("");
+  const [btnStatus, setbtnStatus] = useState(true);
+  const [myid, setMyid] = useState("");
+  const Data = useSelector((state) => state.todo.task);
+  const dispatch = useDispatch();
+  console.log(Data);
 
-    const [val,setVal]=useState("");
-    console.log(work);
-    let sno=0;
-    const ans=work.map((key,index)=>{
-      // sno++;
-      return(
-        <>
-        <tr>
-          <td>{index+1}</td>
-          <td>{key.task}</td>
-          <td><button onClick={()=>{dispatch(RemoveTask({id:key.id}))}}>delete</button></td>
-             
+  const dataEdit = (id, work) => {
+    setTxtVal(work);
+    setbtnStatus(false);
+    setMyid(id);
+  };
 
+  const myEditData = () => {
+    dispatch(myEditSave({ id: myid, work: txtVal }));
+    setbtnStatus(true);
+  };
 
-
-        </tr>
-        </>
-      )
-    })
-    
-    return(
+  let sno = 0;
+  const ans = Data.map((key) => {
+    sno++;
+    return (
       <>
-      <h1>todo program</h1>
-      <h1>enter work:<input type="text" value={val}
-      onChange={(e)=>{setVal(e.target.value)}}/></h1>
-      <button onClick={()=>{dispatch(addtask({task:val}))}}>add</button>
-      <hr/>
-    <table border="5px" width="300px" >
-      <tr>
-        <th>s.no</th>
-        <th>data</th>
-        <th>delete</th>
-      </tr>
-      {ans}
-    </table>
+<tr>
+<td> {sno} </td>
+<td>{key.taskStatus ? (
+<> <span style={{ color: "red", textDecoration: "line-through" }}>{key.work}</span> </> ) : ( <>{key.work} </>)}</td>
+          <td>
+            <button
+              onClick={() => {
+                dispatch(RemoveTask({ id: key.id }));
+              }}
+            >
+              Del
+            </button>
+          </td>
+          <td>
+            <button
+              onClick={() => {
+                dispatch(taskComplete({ id: key.id }));
+              }}
+            >
+              Complete
+            </button>
+          </td>
+          <td>
+            <button
+              onClick={() => {
+                dispatch(taskInComplete({ id: key.id }));
+              }}
+            >
+              InComplete
+            </button>
+          </td>
+          <td>
+            <button
+              onClick={() => {
+                dataEdit(key.id, key.work);
+              }}
+            >
+              Edit
+            </button>
+          </td>
+        </tr>
       </>
-    )
-  }
-  export default App;
+    );
+  });
+  return (
+    <>
+      <h1> ToDo App </h1>
+      Enter Your Task{" "}
+      <input
+        type="text"
+        value={txtVal}
+        onChange={(e) => {
+          setTxtVal(e.target.value);
+        }}
+      />
+      {btnStatus ? (
+        <>
+          <button
+            onClick={() => {
+              dispatch(
+                addTask({ id: Date.now(), work: txtVal, taskStatus: false })
+              );
+            }}
+          >
+            Add
+          </button>
+        </>
+      ) : (
+        <>
+          <button onClick={myEditData}>Edit Save</button>
+        </>
+      )}
+      <hr />
+      <table>
+        <tr>
+          <th> Sno</th>
+          <th> Your Work </th>
+          <th> </th>
+          <th> </th>
+          <th> </th>
+          <th></th>
+        </tr>
+        {ans}
+      </table>
+    </>
+  );
+}
+
+export default App;
--- src/todoslice.jsx
+++ src/todoslice.jsx
@@ -1,19 +1,63 @@
-import { createSlice } from "@reduxjs/toolkit";
-const todoslice=createSlice({
-    name:"todo",
-    initialState:{
-        task:[]
-    },
-    reducers:{
-        addtask:(state,actions)=>{
-            console.log(actions);
-            state.task.push(actions.payload)
+  // \\\\\\\\\\\\\\\\\\\\\\( pay load )\\(addtask)\\(app.jsx,todoslice.jsx,store.jsx)\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+// import { createSlice } from "@reduxjs/toolkit";
+// const todoslice=createSlice({
+//     name:"todo",
+//     initialState:{
+//         task:[]
+//     },
+//     reducers:{
+//         addtask:(state,actions)=>{
+//             console.log(actions);
+//             state.task.push(actions.payload)
             
-        },
-         RemoveTask: (state, actions) => {
+//         }
+//     }
+// })
+// export const  {addtask}=todoslice.actions;
+// export default todoslice.reducer;
+// \\\\\\\\\\\\(delete,del)\\\\\\\\(todo app)\\\\\(app.jsx,todoslice.jsx,store.jsx,main.jsx)\\\\\\\\\\\
+// import { createSlice } from "@reduxjs/toolkit";
+// const todoslice=createSlice({
+//     name:"todo",
+//     initialState:{
+//         task:[]
+//     },
+//     reducers:{
+//         addtask:(state,actions)=>{
+//             console.log(actions);
+//             state.task.push(actions.payload)
+            
+//         },
+//         removetask:(state,actions)=>{
+//             state.task=state.task.filter(item=>item.id!=actions.payload.id)
+//         },
+//         remByIndex:(state,actions)=>{
+//             state.task.splice(actions.payload.id,1);
+//             console.log(actions.payload.id);
+            
+//         }
+//     }
+// })
+// export const  {addtask,removetask, remByIndex}=todoslice.actions;
+// export default todoslice.reducer;
+// \\\\\\\\\\\\\\\\\(delete,complete,incomplete,edit)\\\\(todo app)\\(app.jsx,todoslice.jsx,store.jsx,main.jsx)\\\\\\\\\\\\\\
+
+import { createSlice } from "@reduxjs/toolkit";
+const todoSlice = createSlice({
+  name: "todo",
+  initialState: {
+    task: [],
+  },
+
+  reducers: {
+    addTask: (state, actions) => {
+      state.task.push(actions.payload);
+    },
+    RemoveTask: (state, actions) => {
       state.task = state.task.filter((key) => key.id != actions.payload.id);
     },
-     taskComplete: (state, actions) => {
+    taskComplete: (state, actions) => {
       for (var i = 0; i < state.task.length; i++) {
         if (state.task[i].id == actions.payload.id) {
           state.task[i].taskStatus = true;
@@ -27,7 +71,16 @@ const todoslice=createSlice({
         }
       }
     },
-    }
-})
-export const  {addtask,RemoveTask}=todoslice.actions;
-export default todoslice.reducer;
+    myEditSave: (state, actions) => {
+      for (var i = 0; i < state.task.length; i++) {
+        if (state.task[i].id == actions.payload.id) {
+          state.task[i].work = actions.payload.work;
+        }
+      }
+    },
+  },
+});
+
+export const { addTask, RemoveTask, taskComplete, taskInComplete, myEditSave } =
+  todoSlice.actions;
+export default todoSlice.reducer;
